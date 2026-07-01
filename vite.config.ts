12f5212d7db737bb95ec0a037163f6f0dd60c7d4
Stepan Anchugov <kixxx1@gmail.com>
2026-07-01 16:00:20 +0200

    Set up GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hat/',
   plugins: [react()],
   server: {
     port: process.env.PORT ? Number(process.env.PORT) : 5173,
